89991a4d4742760e6a64ee707faa68a5d2c8df8e
136muneebshouket <92539069+136muneebshouket@users.noreply.github.com>
2023-01-08 18:42:31 +0500

    Update index.js

--- index.js
+++ index.js
@@ -21,9 +21,7 @@ const app = express();
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
-app.use(cors({
-  origin: '*'
-}));
+app.use(cors());
 
 
 
